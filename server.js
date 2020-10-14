8c9f79f6bf88581b0bd47a062ece36e5f5e7e365
Alice Huh <aklee213@gmail.com>
2020-10-14 15:23:34 -0400

    first commit

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/space', (req, res) => {
     res.send('<h1>Space Page<h1>')
 })
 
+app.get('/', (req, res) => {
+    res.send('<h1>Space<h1>')
+})
 
 app.listen(3000, ()=>{
     console.log("I am listening");

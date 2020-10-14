2008853930ffc650933ce1606f11e260dbea0ecb
Sabantu <Sabantuyusuf@gmail.com>
2020-10-14 14:30:34 -0500

    index route

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/space', (req, res) => {
     res.send('<h1>Space Page<h1>')
 })
 
+app.get('/:index', (req, res) => {
+    res.send('<h1>Space Index<h1>')
+})
 
 app.listen(3000, ()=>{
     console.log("I am listening");

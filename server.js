da77a007b3cd1894021004222c995eeb740d01d7
Sabantu <Sabantuyusuf@gmail.com>
2020-10-14 20:44:40 -0500

    Model, views and controllers setup

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express'); 
 const app = express();
+const router = express.Router();
+
+const controller = require('./controller/ctrl')
 
 
+// Set Engine
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(`${__dirname}/public`));
+
+
+//Routers
 
 app.get('/', (req, res) => {
-    res.send('<h1>Space<h1>')
-})
+    res.render('home.ejs');
+});
 
-app.get('/space', (req, res) => {
-    res.send('<h1>Space Page<h1>')
-})
 
-app.get('/:index', (req, res) => {
-    res.send('<h1>Space Index<h1>')
-})
+
 
 app.listen(3000, ()=>{
     console.log("I am listening");

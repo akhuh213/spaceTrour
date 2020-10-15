88925f7253f5f0ac6e6372d87a4c998ddde992ac
Alice Huh <aklee213@gmail.com>
2020-10-14 21:58:34 -0400

    [routes] add routes and views files

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+
 const express = require('express'); 
 const app = express();
+// const session = require('express-session')
+// const SECRET_SESSION = process.env.SECRET_SESSION;
+
+// app.use(require('morgan')('dev'));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(__dirname + '/public'));
+// app.use(layouts);
+// app.use(methodOverride('_method'))
+app.set('view engine', 'ejs');
+
+
+// app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+// app.use(layouts);
+// app.use(methodOverride('_method'))
 
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Space<h1>')
+
+
+    res.render('index')
 })
 
 app.get('/space', (req, res) => {
@@ -15,6 +34,11 @@ app.get('/', (req, res) => {
     res.send('<h1>Space<h1>')
 })
 
+
+
+
 app.listen(3000, ()=>{
     console.log("I am listening");
 });
+
+app.use('/mars', require('./routes/mars'));

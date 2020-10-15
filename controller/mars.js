9f731809e0d9c5a8ffa3f2d36eb8ed245471c39d
Alice Huh <aklee213@gmail.com>
2020-10-15 15:10:12 -0400

    [mars.js] add routes

--- controller/mars.js
+++ controller/mars.js
@@ -2,10 +2,36 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
+//learn about mars page
 router.get('/', (req, res) => {
     res.render('mars')
 
 })
+
+// activities onboard page
+router.get('/onboard', (req, res) => {
+    res.render('onboard')
+
+})
+
+// become a martian page
+router.get('/martian', (req, res) => {
+    res.render('onboard')
+
+})
+
+// life on mars page
+router.get('/life', (req, res) => {
+    res.render('life')
+
+})
+
+//about trip page
+router.get('/aboutTrip', (req, res) => {
+    res.render('aboutTrip')
+
+})
+
 
 
 module.exports = router;
